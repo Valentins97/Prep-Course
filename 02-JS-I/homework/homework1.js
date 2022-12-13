98e77920716653cddaeacd5c8660af382b0c51e8
Valentins97 <106116594+Valentins97@users.noreply.github.com>
2022-12-13 18:18:00 -0500

    Add files via upload

---
+++ 02-JS-I/homework/homework1.js
@@ -0,0 +1,23 @@
+Variable:
+Es una forma de darle un valor a algo, una variable se puede configurar y restablecer. 
+
+para crear una variable en javascript utilizamos la palabra clave var, seguida de un espacio y el 
+nombre de la variable. Tambien se le puede dar un valor usando el signo = . 
+
+Strings: 
+Son bloques de texto y siempre van en comillas, ya sea simple o doble. 
+
+Argumentos:
+Los argumentos sirven para completar una funcion, podemos agregar multiples argumentos colocando
+una coma entre ellos. 
+
+Return: 
+La declaracion return sirve para devolver un valor en especifico.
+
+Declaracion if:
+En pocas palabras sirve para decifrar un valor, "si es asi" o avanzado "si no lo es" para eso se 
+usa la palabra clave if . 
+
+Valores booleanos (true, false): 
+En javascript se usa booleanos para significar verdadero o falso. 
+
