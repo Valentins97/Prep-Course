506874a8b01998785c530f07b3fa7c6ebabcab42
Valentins97 <106116594+Valentins97@users.noreply.github.com>
2022-12-14 16:01:21 -0500

    Add files via upload

---
+++ 03-JS-II/homework2.js
@@ -0,0 +1,18 @@
+for : 
+los bucles for, empiezan con su palabra clave for seguida de parentesis y luego abrir y cerrar
+llaves, dentro de los parentesis se declara una variable esto es sobre lo que se repetira el bucle
+y el ciclo continuara hasta que sea false, si esta programado siempre para que su resultado sea true
+seria un bucle infinito.
+
+&& : 
+es como un comparador entre dos expresiones y su significado es "Y" , si las dos expresiones 
+son iguales su resultado sera "true" si no son iguales sera "false".
+
+|| : 
+es un comparador entre dos expresiones y su significado es "O" , si las dos expresiones o una son verdaderas
+su resultado sera "true" y si las dos expresiones son falsas su resultado sera "false".
+
+! :
+su significado es "NO" , practicamente este operador logico devuelve lo contrario de la expresion, ya sea 
+"true" o "false". 
+
