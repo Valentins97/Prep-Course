8a84fd970a9bbddc6321fdc42a3a030b8e29c343
Valentins97 <106116594+Valentins97@users.noreply.github.com>
2022-12-14 17:11:03 -0500

    Add files via upload

---
+++ 04-JS-III/homework3.js
@@ -0,0 +1,4 @@
+Arrays: 
+Podemos pensar en las matrices como contenedores de almacenamiento para colecciones de datos, 
+construir una matriz es simple, viene conformada por una variable que se establece en [] , 
+luego se agrega un contenedor (separado por coma) tantas cadenas, numeros o booleanos como queramos. 
