5107d234bf3945345c8192c0ec9c9c24e7057552
Valentins97 <106116594+Valentins97@users.noreply.github.com>
2022-12-15 17:17:12 -0500

    Add files via upload

---
+++ 06-JS-V/homework/homework5.js
@@ -0,0 +1,9 @@
+Prototype: 
+Las clases tienen una forma unica de establecer un metodo una vez y dar acceso a cada objeto de esa clase
+a esos metodos. Esto se llama el prototype. Cada clase tiene una propiedad prototype, que luego podemos 
+establecer en metodos. 
+
+Clases:
+Javascript nos da acceso a lo que llamamos un constructor o class. Las clases comparten gran parte de la 
+misma funcionalidad que los objetos normales, pero tambien se expande mucho en esa funcionalidad. Las clases
+son utiles para crear muchos objetos que comparten algunas de las mismas propiedades y metodos. 
