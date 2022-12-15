c6a5f6ca367cd144f29dc84f4ea7cfac093f2014
Valentins97 <106116594+Valentins97@users.noreply.github.com>
2022-12-15 15:42:09 -0500

    Add files via upload

---
+++ 05-JS-IV/homework/homework4.js
@@ -0,0 +1,19 @@
+Objeto:
+El objeto es como un contenedor de datos, los objetos contienen mucha informacion sobre una sola cosa
+los objetos se instancian usando llaves ( {} ). 
+
+Metodos:
+Las funciones guardadas en un objeto se denominan metodos, podemos llamar a un metodo con argumentos
+como lo hariamos con una funcion normal.
+
+Bucle for in: 
+A diferencia del bucle for, en parentesis se denomina una variable se le agrega la palabra "clave in" y el 
+nombre del objeto, esto como resultado nos dara cada clave del objeto  y finalizara cuando haya iterado 
+todas las claves. 
+
+Notacion de puntos:
+Con la notacion de puntos podemos llamar al nombre del objeto, un punto y el nombre de la clave. 
+
+Notacion de corchetes: 
+La notacion de corchetes es como llamar a un elemento en una matriz, se puede llamar a cada clave
+envolviendola en comillas. 
